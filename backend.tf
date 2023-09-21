f70d59c3f2e029c25b06db37e346a145de5b2568
fooweiguo <68820158+fooweiguo@users.noreply.github.com>
2023-09-21 20:15:00 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce3-tfstate-bucket-1"
-    key    = "luqman-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
+    key    = "weiguo-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
     region = "us-east-1"
   }
 }
